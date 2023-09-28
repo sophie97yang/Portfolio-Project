e092414ddfdcd86dfa3ea688b3d9077298461d8a
Sophie Yang <sophie97yang@gmail.com>
2023-09-28 14:35:15 -0700

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+const sessionRouter = require('./session.js');
+const userRouter = require('./users.js');
 
 
 router.use(restoreUser);
+
+router.use('/session',sessionRouter);
+
+router.use('/users',userRouter);
+
+
+//test router
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+  });
 
 module.exports = router;
---
+++ backend/routes/api/session.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+const router = express.Router();
+
+router.post('/', async (req,res,next) => {
+    const { credential, password } = req.body;
+    const user = await User.findOne({
+        where: {
+            [Op.or]: {
+                username: credential,
+                email:credential
+            }
+        },
+        attributes: {
+            include:['id','email','username','hashedPassword']
+        }
+        });
+
+    if (user && bcrypt.compareSync(password,user.hashedPassword.toString())) {
+
+        await setTokenCookie(res,user);
+
+        return res.json({
+            user: {
+                id: user.id,
+                email: user.email,
+                username:user.username
+            }});
+    } else {
+        const error = new Error('Login failed');
+        error.status = 401;
+        error.title = 'Login failed';
+        error.errors = {
+            credential: 'The provided credentials were invalid.'
+        }
+        return next(error);
+    }
+
+})
+
+
+module.exports = router;
