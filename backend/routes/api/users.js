614f55fe41c884098c3a61334f6d7bd2603e3628
Sophie Yang <sophie97yang@gmail.com>
2023-09-28 15:10:55 -0700

    Add User signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,25 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const {setTokenCookie, requireAuth} = require('../../utils/auth.js');
+const {User} = require('../../db/models');
+
 
 const router = express.Router();
+
+router.post('/',async (req,res,next)=> {
+   const {firstName,lastName,email,username,password} = req.body;
+   const saltedAndHashed = bcrypt.hashSync(password);
+   const newUser = await User.create({email,username,hashedPassword:saltedAndHashed});
+   const safeUser = {
+        id: newUser.id,
+        email: newUser.email,
+        username: newUser.username
+    };
+   await setTokenCookie(res,safeUser);
+   res.json({
+    user:safeUser
+});
+})
+
 
 module.exports = router;
