83c56e5613cbed3720acc999cc1b3218edb9487c
Sophie Yang <sophie97yang@gmail.com>
2023-09-29 16:20:03 -0700

    Add Group table migration and seeds

---
+++ backend/db/models/group.js
@@ -0,0 +1,60 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Group extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Group.belongsTo(
+        models.User,
+        {foreignKey:'organizerId'}
+      )
+    }
+  }
+  Group.init({
+    organizerId: {
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull:false,
+      unique:true,
+      validate: {
+        len:[2,60]
+      }
+    },
+    about: {
+      type: DataTypes.TEXT,
+      allowNull:false,
+      validate: {
+        len:[50,2500]
+      }
+    },
+    type: {
+      type:DataTypes.ENUM(['Online','In person']),
+      allowNull:false
+    },
+    private: {
+      type: DataTypes.BOOLEAN,
+      allowNull:false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull:false
+    }
+  }, {
+    sequelize,
+    modelName: 'Group',
+  });
+  return Group;
+};
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(
+        models.Group,
+        { foreignKey:'organizerId',onDelete:'SET NULL',hooks:true }
+      )
     }
   }
   User.init({
