baa168488708a871d543cc26e07c618178569c05
Sophie Yang <sophie97yang@gmail.com>
2023-10-04 08:42:24 -0700

    Add endpoints to get all events and get info about events by group

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,3 +1,53 @@
 const router = require('express').Router();
+const {Event,Group,Venue,User, EventImage} = require('../../db/models');
+const {restoreUser,requireAuth } = require('../../utils/auth.js');
+const {Op} = require('sequelize');
+
+router.get('/', async (req,res,next)=> {
+
+    const events = await Event.findAll({
+        include:[
+        {
+            model:Group,
+            attributes: ['id','name','city','state']
+        },
+        {
+            model:Venue,
+            attributes: ['id','city','state']
+        },
+        {
+            model:User,
+            through: {
+                attributes:['status']
+            }
+        },
+        {
+            model:EventImage
+        }
+    ]
+    });
+
+    const eventsList = [];
+
+    events.forEach(event => eventsList.push(event.toJSON()));
+
+    for (let event of eventsList) {
+        const users = event.Users;
+        const images = event.EventImages;
+        const attending = users.filter((user)=> {
+            return user.Attendance.status === 'attending'
+        });
+        const image = images.filter((image)=> {
+            return image.preview === true
+        });
+        event.previewImage = image[0].url;
+        event.numAttending = attending.length;
+        delete event.Users;
+        delete event.EventImages;
+    }
+
+    res.json(eventsList);
+});
+
 
 module.exports = router;
--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {Group,GroupImage,User,Venue,Membership} = require('../../db/models');
+const {Group,GroupImage,User,Venue,Membership,Event,EventImage} = require('../../db/models');
 const {restoreUser,requireAuth } = require('../../utils/auth.js');
 const {Op} = require('sequelize');
 const membershipRouter = require('./memberships.js');
@@ -411,6 +411,71 @@ router.post('/:groupId/venues',requireAuth,async (req,res,next)=> {
     });
 
 });
+
+
+//events
+
+router.get('/:groupId/events', async (req,res,next)=> {
+    const {groupId} = req.params;
+
+    const group = await Group.findByPk(groupId, {
+        attributes:[],
+        include: {
+            model:Event,
+            include:[
+                {
+                    model:Group,
+                    attributes: ['id','name','city','state']
+                },
+                {
+                    model:Venue,
+                    attributes: ['id','city','state']
+                },
+                {
+                    model:User,
+                    through: {
+                        attributes:['status']
+                    }
+                },
+                {
+                    model:EventImage
+                }
+            ]
+        }
+    });
+
+    if (!group) {
+        const err = new Error("Group couldn't be found");
+        err.title = "Invalid Group Id"
+        err.status=404;
+        return next(err);
+    }
+
+    const groupEvents = group.Events;
+
+    const GroupEventsList = [];
+
+    groupEvents.forEach(event => GroupEventsList.push(event.toJSON()));
+
+    for (let event of GroupEventsList) {
+        const users = event.Users;
+        const images = event.EventImages;
+        const attending = users.filter((user)=> {
+            return user.Attendance.status === 'attending'
+        });
+        const image = images.filter((image)=> {
+            return image.preview === true
+        });
+        event.previewImage = image[0].url;
+        event.numAttending = attending.length;
+        delete event.Users;
+        delete event.EventImages;
+    }
+
+
+    res.json(GroupEventsList);
+});
+
 
 
 module.exports =router;
