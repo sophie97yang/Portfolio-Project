48532190abb3a0b4dc3d5a8f2cb28fc490979901
Sophie Yang <sophie97yang@gmail.com>
2023-10-24 05:57:00 -0700

    More stylings added

--- frontend/src/components/GroupDetails/index.js
+++ frontend/src/components/GroupDetails/index.js
@@ -44,10 +44,10 @@ const GroupDetails = () => {
                     <p>{events.Events.length} {events.Events.length===1 ? 'Event' : 'Events' } · {group.private ? 'Private' : 'Public'}</p>
                     <p> Organized by (insert organizer name here)</p>
                     <div className='gd-manage-buttons'>
-                    <button onClick={handleJoinGroup} className={(sessionUser && sessionUser.id!==group.organizerId) ? 'gd-active' : 'gd-hidden'}>Join This Group</button>
                     <NavLink to={`/groups/${id}/events/new`}> <button className={(sessionUser && sessionUser.id===group.organizerId) ? 'gd-active-organizer' : 'gd-hidden'}>Create An Event</button></NavLink>
                     <NavLink to={`/groups/${id}/edit`}> <button className={(sessionUser && sessionUser.id===group.organizerId) ? 'gd-active-organizer' : 'gd-hidden'}>Update Group</button></NavLink>
                     <button className={(sessionUser && sessionUser.id===group.organizerId) ? 'gd-active-organizer' : 'gd-hidden'}>Delete Group</button>
+                    <button onClick={handleJoinGroup} className={(sessionUser && sessionUser.id!==group.organizerId) ? 'gd-active' : 'gd-hidden'}>Join This Group</button>
                     </div>
                 </div>
             </div>
@@ -83,20 +83,24 @@ const GroupDetails = () => {
 
             {pastEvents.length ? <h3>Past Events <span> ({pastEvents.length}) </span></h3> : <span></span>}
             {pastEvents.length ? pastEvents.map(({id,previewImage,name,startDate,Venue}) => (
-                <NavLink to={`/events/${id}`} key={id} className='group-event-details'>
-                    <img src={previewImage} alt={name}></img>
-                    <div className='ged-right'>
-                        <div className='time'>
-                            <p>{startDate.slice(0,10)} </p>
-                            <p>{startDate.slice(11,16)}</p>
-                        </div>
-                        <h4>{name}</h4>
-                        <p>{Venue.city}, {Venue.state} </p>
-                    </div>
-                    <p>Event Description</p>
-                </NavLink>
-            ))
-            : <span></span>
+                 <div className='group-event-details'>
+                 <NavLink to={`/events/${id}`} key={id}>
+                     <div className='ged-sec-one'>
+                     <img src={previewImage} alt={name}></img>
+                     <div className='ged-right'>
+                         <div className='time'>
+                             <p>{startDate.slice(0,10)} · </p>
+                             <p> {startDate.slice(11,16)}</p>
+                         </div>
+                         <h4>{name}</h4>
+                         <p>{Venue.city}, {Venue.state} </p>
+                     </div>
+                     </div>
+                     <p id='e-description'>Event Description</p>
+                 </NavLink>
+                 </div>
+                 ))
+                 : <span></span>
             }
             </div>
        </div>
